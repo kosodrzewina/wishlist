8afa30283bfea1b1267e22c00d8516450fc92d7d
Piotr Sienkiewicz <sienkiew@live.com>
2022-05-29 17:59:49 +0200

    there's no need for this screen

--- app/src/main/java/com/example/wishlist/navigation/NavGraph.kt
+++ app/src/main/java/com/example/wishlist/navigation/NavGraph.kt
@@ -7,7 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.wishlist.ProductStore
-import com.example.wishlist.screens.CreateOrEditProductScreen
 import com.example.wishlist.screens.HomeScreen
 import com.example.wishlist.screens.ProductDetailScreen
 import java.net.URLDecoder
@@ -35,28 +34,6 @@ fun NavGraph(navHostController: NavHostController) {
             )
 
             ProductDetailScreen(
-                product = ProductStore.products.first { product ->
-                    product.productIdImagePath == decodedProductId
-                },
-                navController = navHostController
-            )
-        }
-
-        composable(
-            route = Screen.CreateOrEditProductScreen.route + "/{encodedProductId}",
-            arguments = listOf(
-                navArgument("encodedProductId") {
-                    type = NavType.StringType
-                    nullable = false
-                }
-            )
-        ) { entry ->
-            val decodedProductId = URLDecoder.decode(
-                entry.arguments?.getString("encodedProductId"),
-                StandardCharsets.UTF_8.toString()
-            )
-
-            CreateOrEditProductScreen(
                 product = ProductStore.products.first { product ->
                     product.productIdImagePath == decodedProductId
                 },
--- app/src/main/java/com/example/wishlist/navigation/Routes.kt
+++ app/src/main/java/com/example/wishlist/navigation/Routes.kt
@@ -2,4 +2,3 @@ package com.example.wishlist.navigation
 
 const val HOME_SCREEN = "home_screen"
 const val PRODUCT_DETAIL_SCREEN = "product_detail_screen"
-const val CREATE_OR_EDIT_PRODUCT_SCREEN = "create_or_edit_product_screen"
